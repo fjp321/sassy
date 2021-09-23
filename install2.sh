cfdaad03cbb13c626b389210b687fa7c20ba37f0
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-09-22 22:56:32 -0400

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,1 +1,10 @@
+#config script and environment
+source config.sh
+source /etc/profile
+export PS1="(chroot) ${PS1}"
 
+#mount to boot
+mount ${disk}1 /boot
+emerge-webrsync
+emerge --sync
+eselect news read
