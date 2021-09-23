cdb27ecf3fff6eb8ff22ee999cd1bff8c1d14d8e
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-09-22 23:08:33 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,4 +41,4 @@ mount --make-rslave /mnt/gentoo/dev
 
 #download next scripts into /mnt/gentoo/
 wget https://raw.github.com/fjp321/fuzzy-goggles/main/install2.sh
-wget https://raw.github.com/fjp321/fuzzy-goggles/main/install2.sh
+wget https://raw.github.com/fjp321/fuzzy-goggles/main/config.sh
