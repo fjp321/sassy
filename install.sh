f3eddb41cef22ea882621d3d197ffca117092548
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-09-22 22:34:31 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,6 +18,35 @@ IFS=' ' read -r stage3location z <<< $(tail -n +3 latest-stage3-amd64-openrc.txt
 IFS='/' read -r z stage3tarball <<< $stage3location
 rm latest-stage3-amd64-openrc.txt
 wget $gentoomirror/releases/amd64/autobuilds/$stage3location
+
+#unzip tar ball
 tar xpvf $stage3tarball
+
+#add options to make conf file
 sed -i 's/COMMON_FLAGS="-O2 -pipe"/COMMON_FLAGS="-O2 -pipe = -march=native"/' etc/portage/make.conf
 echo MAKEOPTS=\"$mkopts\" >> etc/portage/make.conf
+echo GENTOO_MIRRORS=\"$gentoomirror/\" >> etc/portage/make.conf
+
+#add repos conf to portage
+mkdir --parents /mnt/gentoo/etc/portage/repos.conf
+cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
+cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
+
+#mount necessary file systems
+mount --types proc /proc /mnt/gentoo/proc
+mount --rbind /sys /mnt/gentoo/sys
+mount --make-rslave /mnt/gentoo/sys
+mount --rbind /dev /mnt/gentoo/dev
+mount --make-rslave /mnt/gentoo/dev
+
+#change root
+chroot /mnt/gentoo /bin/bash
+source /etc/profile
+export PS1="(chroot) ${PS1}"
+
+#mount boot partition
+mount /dev/sda1 /boot
+
+#install a snapshot of the Gentoo ebuild repository
+emerge-webrsync
+emerge --sync
