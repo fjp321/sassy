9ba1922e5750f6b2a3e914413cac2c3126c0da4d
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-10-07 16:24:58 -0400

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -4,3 +4,6 @@ source config.sh
 mount ${disk}1 /boot
 emerge-webrsync
 emerge --sync
+eselect profile set 5
+USE="-X -cairo -glib -graphite -harfbuzz -icu -introspection -png -truetype" emerge media-libs/freetype media-libs/harfbuzz
+emerge --ask --verbose --update --deep --changed-use @world
