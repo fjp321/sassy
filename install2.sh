3ede9edb8eeda275748fa2e48f2bd1694d754ed4
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-10-12 14:53:45 -0400

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -5,5 +5,9 @@ mount ${disk}1 /boot
 emerge-webrsync
 emerge --sync
 eselect profile set 5
-USE="-X -cairo -glib -graphite -harfbuzz -icu -introspection -png -truetype" emerge media-libs/freetype media-libs/harfbuzz
+USE="abi_x86_32" emerge -v1 sys-libs/zlib
+USE="-* abi_x86_32" emerge --verbose --oneshot --nodeps media-libs/harfbuzz
+USE="-* abi_x86_32" emerge --verbose --oneshot --nodeps media-libs/freetype
+USE="-* truetype harfbuzz abi_x86_32" emerge --verbose --oneshot media-libs/freetype
+emerge --verbose --oneshot media-libs/freetype
 emerge --verbose --update --deep --changed-use --autounmask-write=y @world
