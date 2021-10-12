b2d8f3a1ad31a0d77810604bccd192f78c573910
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-10-12 15:10:36 -0400

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -2,12 +2,16 @@ source config.sh
 
 #mount to boot
 mount ${disk}1 /boot
+#update bins
 emerge-webrsync
 emerge --sync
+#set profile to desktop
 eselect profile set 5
+#fix circ deps
 USE="abi_x86_32" emerge -v1 sys-libs/zlib
 USE="-* abi_x86_32" emerge --verbose --oneshot --nodeps media-libs/harfbuzz
 USE="-* abi_x86_32" emerge --verbose --oneshot --nodeps media-libs/freetype
 USE="-* truetype harfbuzz abi_x86_32" emerge --verbose --oneshot media-libs/freetype
 emerge --verbose --oneshot media-libs/freetype
-emerge --verbose --update --deep --changed-use --autounmask-write=y @world
+#update world
+emerge --verbose --update --deep --changed-use --autounmask-write=y --autounmask-continue=y @world
