365f9f981d43480e275425103225749a6351c46d
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-10-17 15:24:54 -0400

    Update useflags_staging.sh

--- useflags_staging.sh
+++ useflags_staging.sh
@@ -1,5 +1,4 @@
 useflags="x11-wm/openbox www-client/firefox app-emulation/wine-vanilla x11-drivers/xf86-video-amdgpu 
 games-util/steam-meta sys-kernel/linux-firmware dev-vcs/git sci-mathematics/octave x11-misc/dunst 
 x11-misc/polybar x11-misc/dmenu x11-misc/sddm app-editors/vim net-libs/nodejs net-wireless/gnuradio
-net-analyzer/wireshark net-misc/icaclient"
-# useflags for things like x11, dmenu, sddm, vim, nodejs, gnuradio, wireshark, icaclient 
+net-analyzer/wireshark net-misc/icaclient app-emulation/virtualbox"
