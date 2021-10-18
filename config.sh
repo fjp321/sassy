da85f557c90234251584345e6fd3550cc95156d8
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-10-18 16:17:07 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,31 +3,4 @@ bootpar="/dev/sda1"
 swappar="/dev/sda2"
 rootpar="/dev/sda3"
 gentoomirror="https://mirrors.rit.edu/gentoo"
-mkopts="-j2"
-
-useflags="X 
-alsa 
-branding
-capi clang cups custom-cflags 
-debug dbus dos 
-eme-free 
-faudio fontconfig 
-gecko geckodriver gphoto2 gmp-autoupdate gsm gssapi gstreamer 
-hardened hwaccel 
-imlib
-jack jpeg 
-kerberos 
-lcms ldap lto 
-mingw mono mono mp3 
-netapi nls 
-odbc openal opencl opengl openh264 osmesa oss 
-pcap perl pgo png prelink pulseaudio 
-realtime 
-tun-exes 
-samba scanner screencast sdl selinux session sndio ssl startup-notification system-av1 system-harfbuzz system-icu system-jpeg system-libevent system-libvpx system-webp svg
-test threads truetype tun-exes 
-udev udisks unwind usb 
-v4l vkd3d vulkan 
-wayland wifi 
-xcomposite xdg xinerama xml"
-# useflags for things like x11, openbox, firefox, wine
+mkopts="-j4"
