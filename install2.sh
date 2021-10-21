8b1841d908b36eb3b8cbe4dd04e52f283ec79f0c
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-10-21 18:28:15 -0400

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -15,7 +15,7 @@ emerge --verbose --oneshot media-libs/freetype
 #update world
 emerge --verbose --update --deep --changed-use --autounmask-write=y --autounmask-continue=y @world
 
-echo ACCEPT_LICENSE=\"*\" >> /etc/portage/make.conf
+echo -e ACCEPT_LICENSE=\"*\" >> /etc/portage/make.conf
 echo America/New_York > /etc/timezone
 emerge --config sys-libs/timezone-data
 echo en_US ISO-8859-1 >> /etc/locale.gen
@@ -26,3 +26,16 @@ env-update && source /etc/profile
 emerge --autounmask-write=y --autounmask-continue=y sys-kernel/gentoo-sources
 eselect kernel set 1
 emerge --autounmask-write=y --autounmask-continue=y sys-kernel/genkernel
+echo -e "${bootpar}\t/boot\text4\tdefaults,noatime\t0 2"
+genkernel all
+emerge --autounmask-write=y --autounmask-continue=y sys-kernel/linux-firmware
+echo -e "${swappar}\tnone\tswap\tsw\t0 0"
+echo -e "${rootpar}\t/\text4\tnoatime\t0 1"
+sed -i 's/hostname="localhost"/hostname="bristlecone"/' /etc/conf.d/hostname
+emerge --autounmask-write=y --autounmask-continue=y app-admin/sysklogd sys-process/cronie sys-apps/mlocate sys-fs/e2fsprogs sys-fs/dosftools net-misc/dhcpcd net-wireless/iw net-wireless/wpa_supplicant
+rc-update add sysklogd default
+rc-update add cronie default
+echo 'GRUB_PLATFORMS="efi-64"' >> /etc/portage/make.conf
+emerge --autounmask-write=y --autounmask-continue=y sys-boot/grub:2
+grub-install --target=x86_64-efi --efi-directory=/boot
+grub-mkconfig -o /boot/grub/grub.cfg
