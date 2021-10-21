9027ff55e599b153b220660f1d6b6d6b2dcdbf8a
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-10-21 16:28:00 -0400

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -14,3 +14,15 @@ USE="-* truetype harfbuzz abi_x86_32" emerge --verbose --oneshot media-libs/free
 emerge --verbose --oneshot media-libs/freetype
 #update world
 emerge --verbose --update --deep --changed-use --autounmask-write=y --autounmask-continue=y @world
+
+echo ACCEPT_LICENSE=\"*\" >> /etc/portage/make.conf
+echo America/New_York > /etc/timezone
+emerge --config sys-libs/timezone-data
+echo en_US ISO-8859-1 >> /etc/locale.gen
+echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
+locale-gen
+eselect locale set 6
+env-update && source /etc/profile
+emerge --autounmask-write=y --autounmask-continue=y sys-kernel/gentoo-sources
+eselect kernel set 1
+emerge --autounmask-write=y --autounmask-continue=y sys-kernel/genkernel
