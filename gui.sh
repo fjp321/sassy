f59cc7bdf2737b4aa1223beaa90037033cd28d34
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-10-22 14:16:25 -0400

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -2,5 +2,5 @@ emerge --autounmask-write=y --autounmask-continue=y x11-base/xorg-drivers
 env-update
 source /etc/profile
 emerge --autounmask-write=y --autounmask-continue=y gui-libs/display-manager-init
-emerge --autounmask-write=y --autounmask-continue=y x11-misc/sddm
+emerge --autounmask-write=y --autounmask-continue=y x11-misc/lightdm
 rc-update add display-manager default
