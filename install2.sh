74442f66f9cee4ccdb14f94c4c9bb966de2d7eb1
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-10-21 23:00:37 -0400

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -32,10 +32,18 @@ emerge --autounmask-write=y --autounmask-continue=y sys-kernel/linux-firmware
 echo -e "${swappar}\tnone\tswap\tsw\t0 0" >> /etc/fstab
 echo -e "${rootpar}\t/\text4\trw,noatime\t0 1" >> /etc/fstab
 sed -i 's/hostname="localhost"/hostname="bristlecone"/' /etc/conf.d/hostname
-emerge --autounmask-write=y --autounmask-continue=y app-admin/sysklogd sys-process/cronie sys-apps/mlocate sys-fs/e2fsprogs sys-fs/dosftools net-misc/dhcpcd net-wireless/iw net-wireless/wpa_supplicant
+emerge --autounmask-write=y --autounmask-continue=y app-admin/sysklogd 
+emerge --autounmask-write=y --autounmask-continue=y sys-process/cronie 
+emerge --autounmask-write=y --autounmask-continue=y sys-apps/mlocate 
+emerge --autounmask-write=y --autounmask-continue=y sys-fs/e2fsprogs 
+emerge --autounmask-write=y --autounmask-continue=y sys-fs/dosftools 
+emerge --autounmask-write=y --autounmask-continue=y net-misc/dhcpcd 
+emerge --autounmask-write=y --autounmask-continue=y net-wireless/iw 
+emerge --autounmask-write=y --autounmask-continue=y net-wireless/wpa_supplicant
 rc-update add sysklogd default
 rc-update add cronie default
 echo 'GRUB_PLATFORMS="efi-64"' >> /etc/portage/make.conf
 emerge --autounmask-write=y --autounmask-continue=y sys-boot/grub:2
 grub-install --target=x86_64-efi --efi-directory=/boot
 grub-mkconfig -o /boot/grub/grub.cfg
+passwd
