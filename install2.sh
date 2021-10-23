024557d0a597ea971c7052273c9f633b6fef4bf7
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-10-23 18:55:53 -0400

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -14,36 +14,51 @@ USE="-* truetype harfbuzz abi_x86_32" emerge --verbose --oneshot media-libs/free
 emerge --verbose --oneshot media-libs/freetype
 #update world
 emerge --verbose --update --deep --changed-use --autounmask-write=y --autounmask-continue=y @world
-
+#set accept license to all
 echo -e ACCEPT_LICENSE=\"*\" >> /etc/portage/make.conf
+#set timezone to newyork, america
 echo America/New_York > /etc/timezone
 emerge --config sys-libs/timezone-data
+#set lcoale
 echo en_US ISO-8859-1 >> /etc/locale.gen
 echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
 locale-gen
 eselect locale set 6
 env-update && source /etc/profile
+#genkernel
 emerge --autounmask-write=y --autounmask-continue=y sys-kernel/gentoo-sources
 eselect kernel set 1
 emerge --autounmask-write=y --autounmask-continue=y sys-kernel/genkernel
+#setup stab
 echo -e "${bootpar}\t/boot\text4\tdefaults,noatime\t0 2" >> /etc/fstab
 genkernel all
 emerge --autounmask-write=y --autounmask-continue=y sys-kernel/linux-firmware
+#continue fstab setup
 echo -e "${swappar}\tnone\tswap\tsw\t0 0" >> /etc/fstab
 echo -e "${rootpar}\t/\text4\trw,noatime\t0 1" >> /etc/fstab
+#set host name
 sed -i 's/hostname="localhost"/hostname="bristlecone"/' /etc/conf.d/hostname
+#emerge system logger
 emerge --autounmask-write=y --autounmask-continue=y app-admin/sysklogd 
-emerge --autounmask-write=y --autounmask-continue=y sys-process/cronie 
+#emerge cron daemon
+emerge --autounmask-write=y --autounmask-continue=y sys-process/dcron
+#emerge mlocate for file indexing
 emerge --autounmask-write=y --autounmask-continue=y sys-apps/mlocate 
+# emerge fs tools 
 emerge --autounmask-write=y --autounmask-continue=y sys-fs/e2fsprogs 
 emerge --autounmask-write=y --autounmask-continue=y sys-fs/dosftools 
+#emerge dhcpd to get internet and ip assignment
 emerge --autounmask-write=y --autounmask-continue=y net-misc/dhcpcd 
+#emerge wireless internet tools
 emerge --autounmask-write=y --autounmask-continue=y net-wireless/iw 
 emerge --autounmask-write=y --autounmask-continue=y net-wireless/wpa_supplicant
+#add tools to rc
 rc-update add sysklogd default
-rc-update add cronie default
+rc-update add dcron default
+# set up grub
 echo 'GRUB_PLATFORMS="efi-64"' >> /etc/portage/make.conf
 emerge --autounmask-write=y --autounmask-continue=y sys-boot/grub:2
 grub-install --target=x86_64-efi --efi-directory=/boot
 grub-mkconfig -o /boot/grub/grub.cfg
+#set root passwd
 passwd
