9a31f68609fab8fda4e73c7a9a6ee5c7a94643b7
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-10-23 19:28:04 -0400

    Create install_tools.sh

---
+++ install_tools.sh
@@ -0,0 +1,15 @@
+#emerge octave
+emerge --autounmask-write=y --autounmask-continue=y sci-mathematics/octave
+#emerge git
+emerge --autounmask-write=y --autounmask-continue=y dev-vcs/git
+#install wine 
+emerge --autounmask-write=y --autounmask-continue=y virtual/wine
+#install lutris
+
+#setup vim
+
+#setup kitty
+
+#setup polybar
+
+#download zaffiro icons
