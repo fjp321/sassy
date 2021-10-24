23fbc8b99343042a77d8786601dcd22d213ee354
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-10-24 14:31:56 -0400

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -1,30 +1,8 @@
-
-useradd -m -G users,wheel,audio -s /bin/bash fjp
+ https://raw.githubuseradd -m -G users,wheel,audio -s /bin/bash fjp
 passwd fjp
 rm /stage3* /install2.sh /config.sh
-##install lxde
-# base lxde gtk theme
-emerge --autounmask-write=y --autounmask-continue=y lxde-base/lxappearance
-#default config files
-emerge --autounmask-write=y --autounmask-continue=y lxde-base/lxde-common 
-#icon library for lxde
-emerge --autounmask-write=y --autounmask-continue=y lxde-base/lxde-icon-theme
-#lxmenu app menu manaage
-emerge --autounmask-write=y --autounmask-continue=y lxde-base/lxmenu-data
-#keyboard mouse config
-emerge --autounmask-write=y --autounmask-continue=y lxde-base/lxinput
-#polybar bar
-emerge --autounmask-write=y --autounmask-continue=y x11-misc/polybar
-#graphical interface for resie 
-emerge --autounmask-write=y --autounmask-continue=y lxde-base/lxrandr
-#session manager, shitdown reboot suspend etc
-emerge --autounmask-write=y --autounmask-continue=y lxde-base/lxsession 
-#starup applications
-emerge --autounmask-write=y --autounmask-continue=y lxde-base/lxsession-edit 
-#edit application shortcuts
-emerge --autounmask-write=y --autounmask-continue=y lxde-base/lxshortcut
-#task manager
-emerge --autounmask-write=y --autounmask-continue=y lxde-base/lxtask
+# base lxqt base
+emerge --autounmask-write=y --autounmask-continue=y lxqt-base/lxqt-meta
 # terminal emulator 
 emerge --autounmask-write=y --autounmask-continue=y x11-terms/kitty
 # file manager thunar
