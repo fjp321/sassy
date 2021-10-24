82a11d2ee7f93831947768689099b3c6f3d879f5
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-10-24 14:44:05 -0400

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -13,7 +13,7 @@ USE="-* abi_x86_32" emerge --verbose --oneshot --nodeps media-libs/freetype
 USE="-* truetype harfbuzz abi_x86_32" emerge --verbose --oneshot media-libs/freetype
 emerge --verbose --oneshot media-libs/freetype
 #update world
-emerge --verbose --update --deep --changed-use --autounmask-write=y --autounmask-continue=y @world
+emerge --verbose --update --deep --changed-use @world
 #set accept license to all
 echo -e ACCEPT_LICENSE=\"*\" >> /etc/portage/make.conf
 #set timezone to newyork, america
@@ -26,32 +26,32 @@ locale-gen
 eselect locale set 6
 env-update && source /etc/profile
 #genkernel
-emerge --autounmask-write=y --autounmask-continue=y sys-kernel/gentoo-sources
+emerge sys-kernel/gentoo-sources
 eselect kernel set 1
-emerge --autounmask-write=y --autounmask-continue=y sys-kernel/genkernel
+emerge sys-kernel/genkernel
 #setup stab
 echo -e "${bootpar}\t/boot\text4\tdefaults,noatime\t0 2" >> /etc/fstab
 genkernel all
-emerge --autounmask-write=y --autounmask-continue=y sys-kernel/linux-firmware
+emerge sys-kernel/linux-firmware
 #continue fstab setup
 echo -e "${swappar}\tnone\tswap\tsw\t0 0" >> /etc/fstab
 echo -e "${rootpar}\t/\text4\trw,noatime\t0 1" >> /etc/fstab
 #set host name
 sed -i 's/hostname="localhost"/hostname="bristlecone"/' /etc/conf.d/hostname
 #emerge system logger
-emerge --autounmask-write=y --autounmask-continue=y app-admin/sysklogd 
+emerge app-admin/sysklogd 
 #emerge cron daemon
-emerge --autounmask-write=y --autounmask-continue=y sys-process/dcron
+emerge sys-process/dcron
 #emerge mlocate for file indexing
-emerge --autounmask-write=y --autounmask-continue=y sys-apps/mlocate 
+emerge sys-apps/mlocate 
 # emerge fs tools 
-emerge --autounmask-write=y --autounmask-continue=y sys-fs/e2fsprogs 
-emerge --autounmask-write=y --autounmask-continue=y sys-fs/dosftools 
+emerge sys-fs/e2fsprogs 
+emerge sys-fs/dosftools 
 #emerge dhcpd to get internet and ip assignment
-emerge --autounmask-write=y --autounmask-continue=y net-misc/dhcpcd 
+emerge net-misc/dhcpcd 
 #emerge wireless internet tools
-emerge --autounmask-write=y --autounmask-continue=y net-wireless/iw 
-emerge --autounmask-write=y --autounmask-continue=y net-wireless/wpa_supplicant
+emerge net-wireless/iw 
+emerge net-wireless/wpa_supplicant
 #add tools to rc
 rc-update add sysklogd default
 rc-update add dcron default
