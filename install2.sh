20e8409d104982ca8cf346527a751c50cacdce01
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-10-25 13:25:11 -0400

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -9,7 +9,7 @@ eselect profile set 5
 #fix circ deps
 USE="-harfbuzz" emerge -qv --oneshot media-libs/freetype media-libs/harfbuzz =x11-libs/pango-1.48.5-r1
 #update world
-emerge -qv --update --deep --changed-use --autounmask-write=y --autounmask-continue=y @world
+emerge -qv --ask --update --deep --changed-use --autounmask-write=y --autounmask-continue=y @world
 #set accept license to all
 echo -e ACCEPT_LICENSE=\"*\" >> /etc/portage/make.conf
 #set timezone to newyork, america
