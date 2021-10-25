243b77acbb2743b9e86c1494fab6bf452300f2d5
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-10-25 16:58:11 -0400

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -11,7 +11,7 @@ source /etc/profile
 emerge -qv --autounmask-write=y --autounmask-continue=y media-fonts/liberation-fonts
 
 # display manager
-emerge -qv --autounmask-write=y --autounmask-continue=y x11-misc/slim
+emerge -qv --autounmask-write=y --autounmask-continue=y x11-misc/lightdm
 #window manager
 emerge -qv --autounmask-write=y --autounmask-continue=y x11-wm/openbox
 emerge -qv --autounmask-write=y --autounmask-continue=y menumaker
