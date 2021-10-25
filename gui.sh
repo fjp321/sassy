9c69771061749fcf0075e21111527cd22a053acc
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-10-24 21:43:44 -0400

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -1,13 +1,15 @@
 useradd -m -G users,wheel,audio -s /bin/bash fjp
 passwd fjp
 rm /stage3* /install2.sh /config.sh
+#xorg
+emerge -qv --autounmask-write=y --autounmask-continue=y x11-base/xorg-drivers
 # base xfce base
-emerge --autounmask-write=y --autounmask-continue=y xfce-base/xfce4-meta
+emerge -qv --autounmask-write=y --autounmask-continue=y xfce-base/xfce4-meta
 # terminal emulator 
-emerge --autounmask-write=y --autounmask-continue=y x11-terms/kitty
+emerge -qv --autounmask-write=y --autounmask-continue=y x11-terms/kitty
 # file manager thunar
-emerge --autounmask-write=y --autounmask-continue=y xfce-base/thunar
+emerge -qv --autounmask-write=y --autounmask-continue=y xfce-base/thunar
 # display manager
-emerge --autounmask-write=y --autounmask-continue=y x11-misc/sddm
+emerge -qv --autounmask-write=y --autounmask-continue=y x11-misc/sddm
 #window manager
-emerge --autounmask-write=y --autounmask-continue=y x11-wm/openbox
+emerge -qv --autounmask-write=y --autounmask-continue=y x11-wm/openbox
