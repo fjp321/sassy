21785521b44f0861dd877c6c87034a91a75c00f4
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-10-25 19:40:57 -0400

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -1,4 +1,5 @@
-useradd -m -G users,wheel,audio -s /bin/bash fjp
+main_user=fjp
+useradd -m -G users,wheel,audio -s /bin/bash ${main_user}
 passwd ${main_user}
 rm /stage3* /install2.sh /config.sh
 
