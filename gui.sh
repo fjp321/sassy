4d80a29d1f459415fee9872a843a8bdbac362c83
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-10-26 16:07:19 -0400

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -27,6 +27,10 @@ emerge -qv --autounmask-write=y --autounmask-continue=y x11-plugins/docker
 emerge -qv --autounmask-write=y --autounmask-continue=y app-admin/sudo
 echo 'fjp bristlecone /usr/bin/*' >> /etc/sudoers
 
+#wallpapers
+wget https://raw.github.com/fjp321/fuzzy-goggles/main/wallpaper1.png
+wget https://raw.github.com/fjp321/fuzzy-goggles/main/wallpaper2.png
+
 #vim
 emerge -qv --autounmask-write=y --autounmask-continue=y app-editors/vim
 
