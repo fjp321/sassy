9d95460453fc8f8fb9b2b2af1ebcb833f22d630a
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-11-07 13:54:32 -0500

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -7,35 +7,43 @@ mount ${bootpar} /boot
 #update bins
 emerge-webrsync
 emerge --sync
+#set profile to desktop
 eselect profile set 5
+
 #fix circ deps
 USE="-harfbuzz" emerge --autounmask-write=y --autounmask-continue=y --oneshot media-libs/freetype
 #update world
 emerge -qv --update --deep --changed-use --autounmask-write=y --autounmask-continue=y @world
+
 #set accept license to all
 echo -e ACCEPT_LICENSE=\"*\" >> /etc/portage/make.conf
+
 #set timezone to newyork, america
 echo America/New_York > /etc/timezone
 emerge -qv --config sys-libs/timezone-data
-#set lcoale
+
+#set locale
 echo en_US ISO-8859-1 >> /etc/locale.gen
 echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
 locale-gen
 eselect locale set 6
 env-update && source /etc/profile
-#genkernel
+
+#genkernel all !!add custom kernel support!!
 emerge -qv --autounmask-write=y --autounmask-continue=y sys-kernel/gentoo-sources
 eselect kernel set 1
 emerge -qv --autounmask-write=y --autounmask-continue=y sys-kernel/genkernel
-#setup stab
+#setup fstab
 echo -e "${bootpar}\t/boot\text4\tdefaults,noatime\t0 2" >> /etc/fstab
 genkernel all
 emerge -qv sys-kernel/linux-firmware
 #continue fstab setup
 echo -e "${swappar}\tnone\tswap\tsw\t0 0" >> /etc/fstab
 echo -e "${rootpar}\t/\text4\trw,noatime\t0 1" >> /etc/fstab
+
 #set host name
 sed -i 's/hostname="localhost"/hostname="bristlecone"/' /etc/conf.d/hostname
+
 #emerge system logger
 emerge -qv --autounmask-write=y --autounmask-continue=y app-admin/sysklogd
 #emerge cron daemon
@@ -50,13 +58,16 @@ emerge -qv --autounmask-write=y --autounmask-continue=y net-misc/dhcpcd
 #emerge wireless internet tools
 emerge -qv --autounmask-write=y --autounmask-continue=y net-wireless/iw
 emerge -qv --autounmask-write=y --autounmask-continue=y net-wireless/wpa_supplicant
+
 #add tools to rc
 rc-update add sysklogd default
 rc-update add dcron default
+
 # set up grub
 echo 'GRUB_PLATFORMS="efi-64"' >> /etc/portage/make.conf
 emerge -qv --autounmask-write=y --autounmask-continue=y sys-boot/grub:2
 grub-install --target=x86_64-efi --efi-directory=/boot
 grub-mkconfig -o /boot/grub/grub.cfg
+
 #set root passwd
 passwd
