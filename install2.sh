9a50697a616ea995a6160b9b1624153e6743c4e1
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-11-10 13:38:51 -0500

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -15,58 +15,58 @@ USE="-harfbuzz" emerge --autounmask-write=y --autounmask-continue=y --oneshot me
 emerge -qv --update --deep --changed-use --autounmask-write=y --autounmask-continue=y @world
 
 #set accept license to all
-echo -e ACCEPT_LICENSE=\"*\" >> /etc/portage/make.conf
+#echo -e ACCEPT_LICENSE=\"*\" >> /etc/portage/make.conf
 
 #set timezone to newyork, america
-echo America/New_York > /etc/timezone
-emerge -qv --config sys-libs/timezone-data
+#echo America/New_York > /etc/timezone
+#emerge -qv --config sys-libs/timezone-data
 
 #set locale
-echo en_US ISO-8859-1 >> /etc/locale.gen
-echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
-locale-gen
-eselect locale set 6
-env-update && source /etc/profile
+#echo en_US ISO-8859-1 >> /etc/locale.gen
+#echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
+#locale-gen
+#eselect locale set 6
+#env-update && source /etc/profile
 
 #genkernel all !!add custom kernel support!!
-emerge -qv --autounmask-write=y --autounmask-continue=y sys-kernel/gentoo-sources
-eselect kernel set 1
-emerge -qv --autounmask-write=y --autounmask-continue=y sys-kernel/genkernel
+#emerge -qv --autounmask-write=y --autounmask-continue=y sys-kernel/gentoo-sources
+#eselect kernel set 1
+#emerge -qv --autounmask-write=y --autounmask-continue=y sys-kernel/genkernel
 #setup fstab
-echo -e "${bootpar}\t/boot\text4\tdefaults,noatime\t0 2" >> /etc/fstab
-genkernel all
-emerge -qv sys-kernel/linux-firmware
+#echo -e "${bootpar}\t/boot\text4\tdefaults,noatime\t0 2" >> /etc/fstab
+#genkernel all
+#emerge -qv sys-kernel/linux-firmware
 #continue fstab setup
-echo -e "${swappar}\tnone\tswap\tsw\t0 0" >> /etc/fstab
-echo -e "${rootpar}\t/\text4\trw,noatime\t0 1" >> /etc/fstab
+#echo -e "${swappar}\tnone\tswap\tsw\t0 0" >> /etc/fstab
+#echo -e "${rootpar}\t/\text4\trw,noatime\t0 1" >> /etc/fstab
 
 #set host name
-sed -i 's/hostname="localhost"/hostname="bristlecone"/' /etc/conf.d/hostname
+#sed -i 's/hostname="localhost"/hostname="bristlecone"/' /etc/conf.d/hostname
 
 #emerge system logger
-emerge -qv --autounmask-write=y --autounmask-continue=y app-admin/sysklogd
+#emerge -qv --autounmask-write=y --autounmask-continue=y app-admin/sysklogd
 #emerge cron daemon
-emerge -qv --autounmask-write=y --autounmask-continue=y sys-process/dcron
+#emerge -qv --autounmask-write=y --autounmask-continue=y sys-process/dcron
 #emerge mlocate for file indexing
-emerge -qv --autounmask-write=y --autounmask-continue=y sys-apps/mlocate
+#emerge -qv --autounmask-write=y --autounmask-continue=y sys-apps/mlocate
 # emerge fs tools 
-emerge -qv --autounmask-write=y --autounmask-continue=y sys-fs/e2fsprogs
-emerge -qv --autounmask-write=y --autounmask-continue=y sys-fs/dosftools
+#emerge -qv --autounmask-write=y --autounmask-continue=y sys-fs/e2fsprogs
+#emerge -qv --autounmask-write=y --autounmask-continue=y sys-fs/dosftools
 #emerge dhcpd to get internet and ip assignment
-emerge -qv --autounmask-write=y --autounmask-continue=y net-misc/dhcpcd
+#emerge -qv --autounmask-write=y --autounmask-continue=y net-misc/dhcpcd
 #emerge wireless internet tools
-emerge -qv --autounmask-write=y --autounmask-continue=y net-wireless/iw
-emerge -qv --autounmask-write=y --autounmask-continue=y net-wireless/wpa_supplicant
+#emerge -qv --autounmask-write=y --autounmask-continue=y net-wireless/iw
+#emerge -qv --autounmask-write=y --autounmask-continue=y net-wireless/wpa_supplicant
 
 #add tools to rc
-rc-update add sysklogd default
-rc-update add dcron default
+#rc-update add sysklogd default
+#rc-update add dcron default
 
 # set up grub
-echo 'GRUB_PLATFORMS="efi-64"' >> /etc/portage/make.conf
-emerge -qv --autounmask-write=y --autounmask-continue=y sys-boot/grub:2
-grub-install --target=x86_64-efi --efi-directory=/boot
-grub-mkconfig -o /boot/grub/grub.cfg
+#echo 'GRUB_PLATFORMS="efi-64"' >> /etc/portage/make.conf
+#emerge -qv --autounmask-write=y --autounmask-continue=y sys-boot/grub:2
+#grub-install --target=x86_64-efi --efi-directory=/boot
+#grub-mkconfig -o /boot/grub/grub.cfg
 
 #set root passwd
-passwd
+#passwd
