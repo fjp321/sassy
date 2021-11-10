8be161adaa539db550a5a19f584c048c8eff355d
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-11-10 13:39:36 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
-disk="/dev/nvme0n1" #pre partition i.e. sda*, nvme0n1p*
-bootpar="/dev/nvme0n1p1"
-swappar="/dev/nvme0n1p2"
-rootpar="/dev/nvme0n1p3"
+disk="/dev/sda" #pre partition i.e. sda*, nvme0n1p*
+bootpar="/dev/sda1"
+swappar="/dev/sda2"
+rootpar="/dev/sda3"
 gentoomirror="https://mirrors.rit.edu/gentoo"
-mkopts="-j6"
+mkopts="-j4"
 useflags_var="X gtk vulkan bluetooth elogind -systemd -dvd -cdr"
