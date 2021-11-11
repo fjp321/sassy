b61ece77d0d071090fa07ac16e7fb2901161f00e
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-11-11 16:00:16 -0500

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -1,8 +1,8 @@
 main_user=fjp
-useradd -m -G users,wheel,audio,video -s /bin/bash ${main_user}
+useradd -m -G users,wheel,audio,video,input -s /bin/bash ${main_user}
 passwd ${main_user}
 rm /stage3* /install2.sh /config.sh
 
 #wayland
-rc-update add elogind boot
-
+emerge gui-wm/wayfire
+emerge gui-apps/tuigreet
