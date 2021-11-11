6145a639ff71591fa0c5d44680e491b8ac4c3f7f
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-11-11 18:11:27 -0500

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -4,6 +4,10 @@ passwd ${main_user}
 rm /stage3* /install2.sh /config.sh
 
 #wayland gui
+mkdir /home/${main_user}/.config
+emerge -qv --autounmask-write=y --autounmask-continue=y app-eselect/eselect-repository
+eselect repository enable wayland-desktop
+emaint sync --repo wayland-desktop
 emerge -qv --autounmask-write=y --autounmask-continue=y gui-wm/labwc
 
 emerge -qv --autounmask-write=y --autounmask-continue=y x11-misc/sddm
@@ -11,5 +15,3 @@ usermod -a -G video sddm
 sed -i 's/DISPLAYMANAGER="xdm"/DISPLAYMANAGER="sddm"/' /etc/conf.d/display-manager
 rc-update add display-manager default
 rc-update add elogind boot
-mkdir /home/${main_user}/.config
-cp /usr/share/wayfire/wayfire.ini /home/fjp/.config/wayfire.ini
