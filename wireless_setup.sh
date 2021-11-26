28c0aba2958314f19169afa709c85871a335164c
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-11-25 23:12:23 -0500

    Update wireless_setup.sh

--- wireless_setup.sh
+++ wireless_setup.sh
@@ -3,4 +3,6 @@ read -p "what is interface name?" interface
 read -p "whats the ssid?" ssid_name
 read -p "whats the password?" ssid_pass
 
-echo "iw dev ${interface} connect -w ${ssid_name} key 0:${ssid_pass}"
+ip link set dev ${interface} up
+iw dev ${interface} connect -w ${ssid_name} key 0:${ssid_pass}
+
