849594265899ad2f32ebcda1be0e5e9f8e65b426
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-11-26 17:08:10 -0500

    Delete wireless_setup.sh

--- wireless_setup.sh
+++
@@ -1,8 +0,0 @@
-ip address show
-read -p "what is interface name?" interface
-read -p "whats the ssid?" ssid_name
-read -p "whats the password?" ssid_pass
-
-ip link set dev ${interface} up
-iw dev ${interface} connect -w ${ssid_name} key 0:${ssid_pass}
-
