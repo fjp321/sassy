7e45beca95f6b3c00155abefecc6edbda4bb3b14
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-11-25 23:10:30 -0500

    Create wireless_setup.sh

---
+++ wireless_setup.sh
@@ -0,0 +1,6 @@
+ip address show
+read -p "what is interface name?" interface
+read -p "whats the ssid?" ssid_name
+read -p "whats the password?" ssid_pass
+
+echo "iw dev ${interface} connect -w ${ssid_name} key 0:${ssid_pass}"
