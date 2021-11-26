601512e462985b32638fb9d9d8500f96c006bb37
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-11-26 17:17:51 -0500

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -1,22 +1,19 @@
 # as non root
 #emerge octave
-emerge --autounmask-write=y --autounmask-continue=y sci-mathematics/octave
+emerge -qv --autounmask-write=y --autounmask-continue=y sci-mathematics/octave
 #emerge git
-emerge --autounmask-write=y --autounmask-continue=y dev-vcs/git
+emerge -qv --autounmask-write=y --autounmask-continue=y dev-vcs/git
 #install wine 
-emerge --autounmask-write=y --autounmask-continue=y virtual/wine
+emerge -qv --autounmask-write=y --autounmask-continue=y virtual/wine
 #install lutris
-
+emerge -qv --autounmask-write=y --autounmask-continue=y games-util/lutris
 #setup neo vim
-
+emerge -qv --autounmask-write=y --autounmask-continue=y app-editors/neovim
 #setup kitty
-
-#setup polybar
-
-#download zaffiro icons
+emerge -qv --autounmask-write=y --autounmask-continue=y x11-terms/kitty
 
 #get oh my bash
 bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)"
-sed -i 's/OSH_THEME="powerline"/OSH_THEME="sexy"/' /home/fjp/.bashrc
+sed -i 's/OSH_THEME="powerline"/OSH_THEME="demula"/' /home/fjp/.bashrc
 #get sadge
 
