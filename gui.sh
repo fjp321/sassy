2ebea5a94084c0751df92123a9ccf382a840a62d
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-12-20 08:19:33 -0500

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -4,9 +4,5 @@ main_user=fjp
 passwd ${main_user}
 rm /stage3* /install2.sh /config.sh
 
-#add use for mesa
-echo -e "# required by standard X-server installation\nmedia-libs/mesa xa" >> /etc/portage/package.use/mesa 
-emerge --qv --autounmask-write=y --autounmask-continue=y --deep --with-bdeps=y --changed-use --update @world
-
 #packages for gui as well as ee tools
 emerge ${emerge_flags} x11-base/xorg-server polybar iverilog kitty gtkwave www-client/otter dev-vcs/git calcurse media-fonts/hack ripgrep dmenu bspwm feh sxhkd dmenu i3lock octave dev-python/pip app-eselect/eselect-repository rofi dunst picom cmus gimp alsa jupyter
