ee8ac108abce957be91623108204c3eee21cbf84
fjp321 <45053792+fjp321@users.noreply.github.com>
2021-12-31 13:02:32 -0500

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -5,4 +5,14 @@ passwd ${main_user}
 rm /stage3* /install2.sh /config.sh
 
 #packages for gui as well as ee tools
-emerge ${emerge_flags} x11-base/xorg-server x11-drivers/xf86-input-evdev gentoolkit polybar conky neofetch iverilog alacritty gtkwave dev-vcs/git calcurse media-fonts/hack ripgrep dmenu bspwm feh sxhkd i3lock octave dev-python/pip app-eselect/eselect-repository rofi dunst picom cmus gimp alsa jupyter blender neovim
+emerge ${emerge_flags} x11-base/xorg-server x11-drivers/xf86-input-evdev net-libs/webkit-gtk gentoolkit polybar conky neofetch iverilog alacritty gtkwave dev-vcs/git calcurse media-fonts/hack ripgrep dmenu bspwm feh sxhkd i3lock-color octave dev-python/pip rofi dunst picom cmus gimp alsa jupyter blender neovim zathura x11-misc/lightdm-mini-greeter
+
+# light dm set up
+rc-update add dbus default
+rc-update add display-manager default
+# add sed to replace "DISPLAYMANAGER=\"xdm\"" with "DISPLAYMANAGER=\"lightdm\"" at /etc/conf.d/display-manager
+echo -e "greeter-session=lightdm-mini-greeter" >> /etc/lightdm/lightdm.conf
+# add sed to replace "user = CHANGE_ME" to "user = ${main_user}"
+
+# for icons, wget at https://github.com/zayronxio/Zafiro-icons/releases/download/1.2/Zafiro-Icons-Dark.tar.xz
+mkdir /home/fjp/.local/share/icons
