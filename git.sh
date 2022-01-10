549702f1cee469558037444a2f6a61bd05dc5dad
fjp321 <potter.finley@gmail.com>
2022-01-10 11:56:17 -0500

    added steam script, and updated gui script

---
+++ git.sh
@@ -0,0 +1,5 @@
+git clone
+git clone
+git clone
+git clone
+git clone 
--- gui.sh
+++ gui.sh
@@ -4,10 +4,15 @@ main_user=fjp
 passwd ${main_user}
 rm /stage3* /install2.sh /config.sh
 
+# set additional use flags
+echo -e "net-wireless/gnuradio grc uhd modtool" >> /etc/portage/package.use/gnuradio 
+
 #packages for gui as well as ee tools
-emerge ${emerge_flags} x11-base/xorg-server x11-drivers/xf86-input-evdev www-client/links gentoolkit polybar conky neofetch iverilog alacritty gtkwave dev-vcs/git calcurse media-fonts/hack ripgrep dmenu bspwm feh sxhkd i3lock-color octave dev-python/pip rofi dunst picom cmus gimp alsa jupyter blender neovim zathura rstudio
+emerge ${emerge_flags} x11-base/xorg-server x11-drivers/xf86-input-evdev lynx gentoolkit firefox polybar neofetch iverilog alacritty gtkwave dev-vcs/git calcurse media-fonts/hack ripgrep dmenu bspwm feh sxhkd octave lua dev-python/pip rofi dunst picom ncmpcpp gimp alsa jupyter blender zathura =app-editors/neovim-0.6.0 mpv youtube-dl neomutt hsetroot vnstat app-eselect/eselect-repository =net-wireless/gnuradio-9999 
 
-# add qingy support
-
-# for icons, wget at https://github.com/zayronxio/Zafiro-icons/releases/download/1.2/Zafiro-Icons-Dark.tar.xz
-mkdir /home/fjp/.local/share/icons
+# mkdir home directories
+mkdir /home/${main_user}/music
+mkdir /home/${main_user}/down
+mkdir /home/${main_user}/doc
+mkdir /home/${main_user}/script
+mkdir /home/${main_user}/git
