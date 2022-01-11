d8267570d913108067781265fea0623d459ceab2
fjp321 <potter.finley@gmail.com>
2022-01-11 13:03:56 -0500

    added scripts

---
+++ scripts/logout.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Logout using dmenu
+
+chosen=$(echo -e "Logout\nReboot\nShutdown" | rofi -show drun -show-icons -dmenu -i)
+
+if [[ $chosen = "Logout" ]]; then
+    kill -9 -1
+elif [[ $chosen = "Shutdown" ]]; then
+	poweroff
+elif [[ $chosen = "Reboot" ]]; then
+	reboot
+fi
