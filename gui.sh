367533bc52a1d4e42798979b128830466b4b766e
fjp321 <45053792+fjp321@users.noreply.github.com>
2022-02-23 18:41:36 -0500

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -4,8 +4,8 @@ useradd --create-home --groups tty,users,wheel,portage,lp,adm,audio,cdrom,disk,u
 passwd ${main_user}
 rm /stage3* /install2.sh /config.sh
 
-#packages for gui as well as ee tools
-emerge ${emerge_flags} x11-base/xorg-server x11-drivers/xf86-input-evdev lynx gentoolkit firefox polybar neofetch iverilog kitty gtkwave dev-vcs/git calcurse ripgrep feh octave lua dev-python/pip ncmpcpp gimp blender libre-of app-editors/neovim mpv youtube-dl neomutt hsetroot vnstat app-eselect/eselect-repository awesome lightdm xclip mpd mpc libreoffice rofi 
+#packages for gui
+emerge ${emerge_flags} x11-base/xorg-server x11-drivers/xf86-input-evdev lynx gentoolkit firefox neofetch x11-terms/st dev-vcs/git calcurse feh dev-python/pip libreoffice app-editors/vim neomutt app-eselect/eselect-repository lightdm openbox
 
 #note; use cordless as client for discord
 
@@ -18,5 +18,3 @@ mkdir /home/${main_user}/down
 mkdir /home/${main_user}/doc
 mkdir /home/${main_user}/script_dev
 mkdir /home/${main_user}/git
-
-su ${main_user}
