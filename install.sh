e0dc6fe88cd98e19ebdbf155a7aaee3d547c9cfa
fjp321 <45053792+fjp321@users.noreply.github.com>
2022-03-01 12:56:27 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -90,21 +90,23 @@ while getopts "dwang" options; do
         esac
 done
 
-if [ $amd_flag = 1 ] && [ $nvidia_flag = 0 ] && [ $intel_flag = 0 ] 
-then
-        video_card="amdgpu radeonsi"
-fi
-if [ $amd_flag = 0 ] && [ $nvidia_flag = 1 ] && [ $intel_flag = 0 ] 
-then
-        video_cards="nvidia nouveau"
-fi
-if [ $amd_flag = 0 ] && [ $nvidia_flag = 0 ] && [ $intel_flag = 1 ] 
-then
-        video_cards="intel"
-fi
 
 if [ fast_flag = 0 ] 
 then
+
+        
+        if [ $amd_flag = 1 ] && [ $nvidia_flag = 0 ] && [ $intel_flag = 0 ] 
+        then
+                video_card="amdgpu radeonsi"
+        fi
+        if [ $amd_flag = 0 ] && [ $nvidia_flag = 1 ] && [ $intel_flag = 0 ] 
+        then
+                video_cards="nvidia nouveau"
+        fi
+        if [ $amd_flag = 0 ] && [ $nvidia_flag = 0 ] && [ $intel_flag = 1 ] 
+        then
+                video_cards="intel"
+        fi
 
         # specify device
         printf "Specify device to write to (press enter for default /dev/sda) > "
