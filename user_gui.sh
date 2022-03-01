a44c5c75c6a79b8c07dbb6d52d34a38c07fcfe6b
fjp321 <45053792+fjp321@users.noreply.github.com>
2022-03-01 13:36:40 -0500

    Update user_gui.sh

--- user_gui.sh
+++ user_gui.sh
@@ -1,3 +1,6 @@
+emerge -qv --autounmask=y --autounmask-write=y --autounmask-continue=y x11-base/xorg-server x11-drivers/xf86-input-evdev firefox neofetch x11-terms/st calcurse feh dev-python/pip libreoffice app-editors/vim neomutt app-eselect/eselect-repository lightdm openbox
+rc-update add elogind boot
+
 mkdir doc
 mkdir down
 mkdir git
