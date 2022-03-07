907cc27b69819ee092ed81081169f4e6dc7e0a1e
fjp321 <45053792+fjp321@users.noreply.github.com>
2022-03-07 17:18:49 -0500

    Update user_gui.sh

--- user_gui.sh
+++ user_gui.sh
@@ -1,6 +1,8 @@
-emerge -qv --autounmask=y --autounmask-write=y --autounmask-continue=y x11-base/xorg-server x11-drivers/xf86-input-evdev firefox neofetch x11-terms/st calcurse feh dev-python/pip libreoffice app-editors/vim neomutt app-eselect/eselect-repository lightdm openbox
-rc-update add elogind boot
-
+emerge -qv --autounmask-write=y --autounmask-continue=y x11-base/xorg-server x11-drivers/xf86-input-evdev firefox neofetch x11-terms/st calcurse feh dev-python/pip libreoffice app-editors/vim neomutt app-eselect/eselect-repository lightdm openbox x11-misc/lightdm-mini-greeter
+mkdir /etc/share/wallpapers && cd /etc/share/wallpapers
+wget https://raw.github.com/fjp321/sassy/main/extra/cyber_porsche.png
+wget https://raw.github.com/fjp321/sassy/main/extra/cyber_porsche_blur.png
+  
 mkdir doc
 mkdir down
 mkdir git
