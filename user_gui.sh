52d0e3d24e08da4efa59db771f80e04877b5e5b1
fjp321 <45053792+fjp321@users.noreply.github.com>
2022-03-08 14:25:49 -0500

    Update user_gui.sh

--- user_gui.sh
+++ user_gui.sh
@@ -1,10 +1,5 @@
+# do things as root
 emerge -qv --autounmask-write=y --autounmask-continue=y x11-base/xorg-server x11-drivers/xf86-input-evdev firefox neofetch x11-terms/st calcurse feh dev-python/pip libreoffice app-editors/vim neomutt app-eselect/eselect-repository lightdm openbox x11-misc/lightdm-mini-greeter
 mkdir /etc/share/wallpapers && cd /etc/share/wallpapers
 wget https://raw.github.com/fjp321/sassy/main/extra/cyber_porsche.png
 wget https://raw.github.com/fjp321/sassy/main/extra/cyber_porsche_blur.png
-  
-mkdir doc
-mkdir down
-mkdir git
-mkdir music
-mkdir pics
