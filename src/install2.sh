d69e0325b2c74bc01093877ddfdc28f71f487953
fjp321 <45053792+fjp321@users.noreply.github.com>
2022-08-09 16:47:39 -0400

    Update install2.sh

--- src/install2.sh
+++ src/install2.sh
@@ -98,11 +98,3 @@ useradd --create-home --groups tty,users,wheel,portage,lp,adm,audio,cdrom,disk,u
 echo "THIS IS ${main_user} PASSWD"
 passwd ${main_user}
 
-if [ $gui_flag = 1 ]
-then
-	emerge -qv --autounmask-write=y --autounmask-continue=y x11-base/xorg-server x11-drivers/xf86-input-evdev firefox neofetch x11-terms/st calcurse feh dev-python/pip libreoffice app-editors/vim neomutt app-eselect/eselect-repository lightdm openbox x11-misc/lightdm-mini-greeter
-	mkdir /etc/share/wallpapers && cd /etc/share/wallpapers
-	wget https://raw.github.com/fjp321/sassy/main/extra/cyber_porsche.png
-	wget https://raw.github.com/fjp321/sassy/main/extra/cyber_porsche_blur.png
-	
-fi
