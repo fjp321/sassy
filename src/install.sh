79fa45672ae5a6bb617a77d20ff607d386937ddd
fjp321 <45053792+fjp321@users.noreply.github.com>
2023-08-02 11:30:49 -0400

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -12,22 +12,6 @@ main(){
         ntpd -q -g
 }
 
-# partition selected disk, will return the partitioned disk
-partition(){
-       DISK = select_disk 
-       ROOT_PAR = ''
-       BOOT_PAR = ''
-       if [[ $DISK == *"sda"* ]]; then
-                ROOT_PAR = "${DISK}2"
-                BOOT_PAR = "${DISK}1"
-       else 
-                ROOT_PAR = "${DISK}p2"
-                BOOT_PAR = "${DISK}p1"
-       fi
-       echo "Partitioning ${DISK} with partitions $BOOT_PAR, $ROOT_PAR using parted"
-       parted /dev/$DISK mklabel gpt mkpart "EFI system partition" fat32 1MiB 513MiB set 1 esp on mkpart "home partition" ext4 513MiB 100%
-}
-
 # select the disk
 select_disk(){
         DISK_TEMP = ''
@@ -40,6 +24,21 @@ select_disk(){
                         DISK=$DISK_TEMP
                 fi
         done
+}
+
+# partition selected disk, will return the partitioned disk
+partition(){
+       ROOT_PAR = ''
+       BOOT_PAR = ''
+       if [[ $DISK == *"sda"* ]]; then
+                ROOT_PAR = "${DISK}2"
+                BOOT_PAR = "${DISK}1"
+       else 
+                ROOT_PAR = "${DISK}p2"
+                BOOT_PAR = "${DISK}p1"
+       fi
+       echo "Partitioning ${DISK} with partitions $BOOT_PAR, $ROOT_PAR using parted"
+       parted /dev/$DISK mklabel gpt mkpart "EFI system partition" fat32 1MiB 513MiB set 1 esp on mkpart "home partition" ext4 513MiB 100%
 }
 
 # make fs on partitions
